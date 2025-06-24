f8320b8c29630b0e89f6410d16b81a1d91af7d42
ly-sir <140997092+ly-sir@users.noreply.github.com>
2025-06-25 04:48:11 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from qiskit import QuantumCircuit
 
 def run_autotuning_transpilation(qc):
     device_cuda = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_path = f"./train/egreedy/model/node_gate_num/GAT_learn_rate_000001_30000_net_256_relu_memory_30000_batch_128_e_greedy_O3_preprocessing/_250000/policy_model.pth"
+    model_path = f"./train/e_greedy/model/node_gate_num/GAT_learn_rate_000001_30000_net_256_relu_memory_30000_batch_128_e_greedy_O3_preprocessing/_250000/policy_model.pth"
     q_network = torch.load(model_path, map_location=device_cuda)
     q_network.eval()
     reward_function = "node_gate_num"
@@ -34,7 +34,7 @@ def run_autotuning_transpilation(qc):
 
 # demo
 
-qasm_path = "./new_circuits/MQTBench_2_30_train/graphstate_indep_tket_20.qasm"
+qasm_path = "./circuits/MQTBench_2_30_train/graphstate_indep_tket_20.qasm"
 ciruit = QuantumCircuit.from_qasm_file(qasm_path)
 
 compiled_circuit, seq = run_autotuning_transpilation(ciruit)
